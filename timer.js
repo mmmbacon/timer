ae771199a9447fe4a92ef0a79a03339b68dfe089
Brandon <bmacdonald1986@gmail.com>
2021-04-06 22:45:21 +0000

    added timer.js to use process.argv as input

---
+++ timer.js
@@ -0,0 +1,15 @@
+const args = process.argv.slice(2);
+
+if (Array.isArray(args)) {
+  for (let x = 0; x < args.length; x++) {
+
+    let val = parseInt(args[x]);
+
+    if (Number.isInteger(val) && val > 0) {
+      setTimeout(()=>{
+        process.stdout.write('\x07');
+        console.log('beep');
+      },(val * 1000));
+    }
+  }
+}
